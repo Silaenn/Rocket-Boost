9a8a531a020b301b192efe77e9dd81bec082d7b4
Deo silaen <deokeldisilaen@gmail.com>
2024-12-24 14:22:24 +0700

    addAudioControl

--- Rocket Boost/Assets/Scripts/Movement.cs
+++ Rocket Boost/Assets/Scripts/Movement.cs
@@ -8,8 +8,10 @@ public class Movement : MonoBehaviour
     [SerializeField] float rotationStrength = 100f;
 
     Rigidbody rb;
+    AudioSource audioSource;
     private void Start() {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnEnable() {
@@ -28,6 +30,11 @@ public class Movement : MonoBehaviour
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+            if(!audioSource.isPlaying){
+            audioSource.Play();
+            }
+        } else {
+            audioSource.Stop();
         }
     }
 

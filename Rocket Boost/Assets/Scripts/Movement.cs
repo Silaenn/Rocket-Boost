a1852e57a11d247cd9926cde35b686361089e332
Deo silaen <deokeldisilaen@gmail.com>
2024-12-24 14:12:59 +0700

    addAudio

--- Rocket Boost/Assets/Scripts/Movement.cs
+++ Rocket Boost/Assets/Scripts/Movement.cs
@@ -45,6 +45,8 @@ public class Movement : MonoBehaviour
 
     private void ApplayRotation(float rotationThisFrame)
     {
+        rb.freezeRotation = true;
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = false;
     }
 }

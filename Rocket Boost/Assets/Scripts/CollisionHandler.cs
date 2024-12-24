9d044e580e04b57b35cc68705668b0e0dbacb999
Deo silaen <deokeldisilaen@gmail.com>
2024-12-24 15:21:24 +0700

    LoadSceneNext

--- Rocket Boost/Assets/Scripts/CollisionHandler.cs
+++ Rocket Boost/Assets/Scripts/CollisionHandler.cs
@@ -7,13 +7,22 @@ public class CollisionHandler : MonoBehaviour
         switch(other.gameObject.tag){
             case "Friendly": Debug.Log("Everything is looking good!");; break;
             case "Fuel": Debug.Log("You're all done, welcome to our country"); break;
-            case "Finish": Debug.Log("Why did you pick me up. I'm not in this game"); break;
+            case "Finish": LoadNextScene(); break;
             default: ReloadLevel(); break;
         }
 
         void ReloadLevel(){
             int currentScene = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(currentScene);
+        }
+        void LoadNextScene(){
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            int nextScene = currentScene + 1;
+
+            if(nextScene == SceneManager.sceneCountInBuildSettings){
+                nextScene = 0;
+            }
+            SceneManager.LoadScene(nextScene);
         }
     }
 }
